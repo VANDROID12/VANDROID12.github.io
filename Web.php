79631780e1d5e240661dd0b65ce6420fb84f9d0d
VANDROID12 <78011965+VANDROID12@users.noreply.github.com>
2021-08-21 13:18:30 +0530

    Create Web.php

---
+++ Web.php
@@ -0,0 +1,14 @@
+<html>
+	<body>
+		<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    	$name = $_POST['fname'];
+    	if (empty($name)) {
+      	echo "empty";
+    } else {
+      	echo $name;
+    }
+  }
+  ?>
+	</body>
+</html>
