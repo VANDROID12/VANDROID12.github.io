6a6f0c4fd34ae837f68ff2b585f89a65f7054c40
VANDROID12 <78011965+VANDROID12@users.noreply.github.com>
2021-08-21 13:42:53 +0530

    Update Web.php

--- Web.php
+++ Web.php
@@ -1,14 +1,21 @@
 <html>
 	<body>
 		<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    	$name = $_POST['fname'];
-    	if (empty($name)) {
-      	echo "empty";
-    } else {
-      	echo $name;
-    }
-  }
-  ?>
+		$host="remotemysql.com";
+    		$user="GWY4IGh2bV";
+    		$password="ZPwVwUyVec";
+    		$database="GWY4IGh2bV";
+		
+		$connection = mysqli_connect($host , $user , $password , $database);
+		
+		$sql = "INSERT INTO messages (name , message) VALUES ('Dubai' ,'busy')";
+		
+		if (mysqli_query($connection , $sql)) {
+			echo"new record created";
+		} else {
+			echo"error";
+		}
+		mysqli.close($connection);
+		?>
 	</body>
 </html>
